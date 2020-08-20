f06380f7661a2fb9519130d9b8906b969ccb2463
root <zalbinaridwan@gmail.com>
2020-08-20 12:57:40 +0700

    footers

--- application/views/commerce/front/footer.php
+++ application/views/commerce/front/footer.php
@@ -16,7 +16,7 @@
         </div>
     </div>
 </section>
-<section class="section_footer">
+<section class="section_footer" style="backgroun:#f1f1f1;">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
