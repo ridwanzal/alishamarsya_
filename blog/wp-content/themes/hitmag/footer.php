988b9e3acde6063f96369835e5ede2b6573b1855
root <root@localhost.localdomain>
2021-08-02 02:52:04 +0000

    add new assets

---
+++ blog/wp-content/themes/hitmag/footer.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package HitMag
+ */
+
+?>
+	</div><!-- .hm-container -->
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="hm-container">
+			<div class="footer-widget-area">
+				<div class="footer-sidebar" role="complementary">
+					<?php if ( ! dynamic_sidebar( 'footer-left' ) ) : ?>
+						
+					<?php endif; // end sidebar widget area ?>
+				</div><!-- .footer-sidebar -->
+		
+				<div class="footer-sidebar" role="complementary">
+					<?php if ( ! dynamic_sidebar( 'footer-mid' ) ) : ?>
+
+					<?php endif; // end sidebar widget area ?>
+				</div><!-- .footer-sidebar -->		
+
+				<div class="footer-sidebar" role="complementary">
+					<?php if ( ! dynamic_sidebar( 'footer-right' ) ) : ?>
+
+					<?php endif; // end sidebar widget area ?>
+				</div><!-- .footer-sidebar -->			
+			</div><!-- .footer-widget-area -->
+		</div><!-- .hm-container -->
+
+		<div class="site-info">
+			<div class="hm-container">
+				<div class="site-info-owner">
+					<?php
+						$footer_copyright_text = get_theme_mod( 'footer_copyright_text', '' );
+
+						if ( ! empty ( $footer_copyright_text ) ) {
+							echo wp_kses_post( $footer_copyright_text );
+						} else {
+							$site_link = '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" >' . esc_attr( get_bloginfo( 'name' ) ) . '</a>';
+							printf( esc_html__( 'Copyright &#169; %1$s %2$s.', 'hitmag' ), date_i18n( 'Y' ), $site_link );
+						}		
+					?>
+				</div>			
+				<div class="site-info-designer">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hitmag' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'hitmag' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'hitmag' ), 'HitMag', '<a href="https://themezhut.com/themes/hitmag/" rel="designer">ThemezHut</a>' ); ?>
+				</div>
+			</div><!-- .hm-container -->
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
